03f7c2fc6cb5fadcf0ef1a77c4a3102b1febbb1d
Amrit Prusty <amrit2995@gmail.com>
2024-01-08 07:53:45 +0530

    initial commmits

--- server/library/api.py
+++ server/library/api.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+# from rest_framework
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics, status
+from .models import *
+from .serializers import *
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+# @api_view(['POST'])
+# def create_author(request):
+#     if request.method == 'POST':
+
+
+class PersonListCreateView(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+class PersonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+
+@csrf_exempt
+@api_view(['POST'])
+def create_person(request):
+    if request.method == "POST":
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            new_person = Person(**serializer.validated_data)
+            new_person.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"error":"Invalid request method"}, status=status.HTTP_400_BAD_REQUEST)
--- server/library_server/settings.py
+++ server/library_server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
     "library"
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 ROOT_URLCONF = "library_server.urls"
@@ -123,3 +126,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
